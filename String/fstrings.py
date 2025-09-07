a1c31fb0e4184614c803419355561ca85bf8ba50
komalk <komalk@gmail.com>
2025-09-07 00:43:49 -0230

    f-strings in Python

---
+++ String/fstrings.py
@@ -0,0 +1,21 @@
+# String Formatting
+''' String formatting is a powerful feature in Python that allows you to insert variables
+ and expressions into strings in a structured way. Python provides multiple ways to
+ format strings, including the older .format() method and the modern f-strings .'''
+
+template = "Dear {}, You are so adorable. Take this {} bag"
+a = "Jack"
+a1 = "Ryan"
+b = 300
+b1 = 1000
+c = "Sameer"
+d = "chocolate"
+
+print(f"{a1} You are adorable please take this {d} bag") 
+
+
+''' ord() and chr(), character encoding:
+print(ord('A')) # Output: 65
+print(chr(65)) # Output: 'A' '''
+
+
